c19fd712d8ad3f83b5e329b11841989f9d9b43eb
Akisanya <akinakisanya@gmail.com>
2021-10-19 08:41:06 -0400

    adding hello world for microcontroller

---
+++ Hello_World.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(9) == HIGH) {
+    digitalWrite(8, HIGH);
+    delay(1000);
+    digitalWrite(8, LOW);
+    delay(1000);
+  } else {
+    digitalWrite(8, LOW);
+  }
+}
